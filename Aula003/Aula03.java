c917c2c77793dcae49596b854032ffeac4d85702
LucasMoraca <lucaskmoraca@gmail.com>
2025-07-10 22:44:54 -0300

    Aula03
    Exercício da aula 3

---
+++ Aula003/Aula03.java
@@ -0,0 +1,12 @@
+package Aula003;
+
+public class Aula03 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.modelo = "BIC cristal";
+        c1.cor = "Azul";
+        c1.carga = 80;
+        c1.tampar();
+        c1.status();
+    }
+}
