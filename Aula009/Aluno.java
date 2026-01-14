45566ce8a0a178cf084fe6f75119daf097f37125
LucasMoraca <lucaskmoraca@gmail.com>
2026-01-14 17:39:11 -0300

    Herança
    Aplicando a base introdutória de classes heranças em java, criando classes com similaridades fixa em outra classe.

---
+++ Aula009/Aluno.java
@@ -0,0 +1,26 @@
+package Aula009;
+
+public class Aluno extends Pessoa {
+    private int mat;
+    private String curso;
+
+    public void cancelarMatricula() {
+        System.out.println("Matrícula cancelada.");
+    }
+
+    // Getters and Setters
+    public int getMat() {
+        return mat;
+    }
+    public void setMat(int mat) {
+        this.mat = mat;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+}
---
+++ Aula009/Professor.java
@@ -0,0 +1,25 @@
+package Aula009;
+
+public class Professor extends Pessoa {
+    private String especialidade;
+    private float salario;
+
+    public void receberAumento(float aumento) {
+        this.salario += aumento;
+    }
+
+    // Getters and Setters
+    public String getEspecialidade() {
+        return especialidade;
+    }
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+}
